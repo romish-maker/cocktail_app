ce307107cb2225f9b6117445ee61214b1b39f4d2
romishkuvatov <romish@mykeruen.kz>
2022-10-10 17:49:21 +0600

    add useAppDispatch useAppSelector hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,5 @@
-export * from './useCreation'
-export * from './useDebounceFn'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import {AppDispatch, RootState} from "../store";
+
+export const useAppDispatch = () => useDispatch<AppDispatch>()
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
